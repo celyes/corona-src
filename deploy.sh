7b08664c7679a6e31841a024e58e93d39bdef0d4
celyes <celyes02@gmail.com>
2020-03-17 01:47:41 +0100

    fix chart issue

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+npm run webpack
+git add .
+git commit -m "$1"
+git push origin master
--- src/js/main.js
+++ src/js/main.js
@@ -7,17 +7,17 @@ fetch("https://corona.lmao.ninja/countries/algeria").then((response) => {
         deaths = document.querySelector("#deaths"),
         recovered = document.querySelector("#recovered"),
         todayCases = document.querySelector("#todayCases");
-        cases.innerText = data.cases;
+        cases.innerText = data.cases - (data.deaths + data.recovered);
         deaths.innerText = data.deaths;
         recovered.innerText = data.recovered;
         todayCases.innerText = data.todayCases;
         ctx = document.getElementById("myChart").getContext("2d"),
         data = {
-            labels: ["الحالات المؤكدة","الحالات المتوفاة", "الحالات التي شفيت", "الحالات الجديدة"],
+            labels: ["الحالات قيد التماثل للشفاء","الحالات المتوفاة", "الحالات التي شفيت", "الحالات الجديدة"],
             datasets: [{
                 label: "الاصابات (الحالات)",
                 backgroundColor: ["#dc3545", "#ffc107", "#0fd850", "#007BFF"],
-                data: [data.cases, data.deaths, data.recovered, data.todayCases]
+                data: [data.cases - (data.deaths + data.recovered), data.deaths, data.recovered, data.todayCases]
             }]
         },
         options = {},
