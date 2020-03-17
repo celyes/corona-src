9fab2404765782eb09208f4b0b24b79ea33f3e53
celyes <celyes02@gmail.com>
2020-03-17 01:00:21 +0100

    fix js bugs

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,11 @@ fetch("https://corona.lmao.ninja/countries/algeria").then((response) => {
     let cases = document.querySelector("#cases"),
         deaths = document.querySelector("#deaths"),
         recovered = document.querySelector("#recovered"),
-        todayCases = document.querySelector("#todayCases"),
-        cases.innerText = data.cases,
-        deaths.innerText = data.deaths,
-        recovered.innerText = data.recovered,
-        todayCases.innerText = data.todayCases,
+        todayCases = document.querySelector("#todayCases");
+        cases.innerText = data.cases;
+        deaths.innerText = data.deaths;
+        recovered.innerText = data.recovered;
+        todayCases.innerText = data.todayCases;
         ctx = document.getElementById("myChart").getContext("2d"),
         data = {
             labels: ["الحالات المؤكدة", "الحالات المتوفاة", "الحالات التي شفيت", "الحالات الجديدة"],
