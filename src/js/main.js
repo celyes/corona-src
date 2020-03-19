2259c1f850101d7eba1cfb9d51a5f620aee4b5e0
celyes <celyes02@gmail.com>
2020-03-19 12:48:17 +0100

    fix chart issue

--- src/js/main.js
+++ src/js/main.js
@@ -7,34 +7,22 @@ fetch("https://corona.lmao.ninja/countries/algeria").then((response) => {
   return response.json();
 }).then((data) => {
   // data
-      cases = document.querySelector("#cases"),
-      deaths = document.querySelector("#deaths"),
-      total = document.querySelector("#total"),
-      recovered = document.querySelector("#recovered"),
-      todayCases = document.querySelector("#todayCases");
+
+  let cases = document.querySelector("#cases"),
+  deaths = document.querySelector("#deaths"),
+  total = document.querySelector("#total"),
+  recovered = document.querySelector("#recovered"),
+  todayCases = document.querySelector("#todayCases");
 
   cases.innerText = data.cases - (data.deaths + data.recovered);
   deaths.innerText = data.deaths;
   total.innerText = "عدد الحالات الإجمالي: " + data.cases + " حالة ";
   recovered.innerText = data.recovered;
   todayCases.innerText = data.todayCases;
-  // chart
-  ctx = document.getElementById("myChart").getContext("2d");
 
-  chartData = {
-      labels: ["الحالات قيد التماثل للشفاء","الحالات المتوفاة", "الحالات التي شفيت", "الحالات الجديدة"],
-      datasets: [{
-          label: "الاصابات (الحالات)",
-          backgroundColor: ["#007BFF", "#ffc107", "#0fd850", "#dc3545"],
-          data: [data.cases - (data.deaths + data.recovered), data.deaths, data.recovered, data.todayCases]
-      }]
-  },
-  myChart = new Chart(ctx, {
-    type: "doughnut",
-    data: chartData,
-    options: {}
-  });
 });
+
+// chart
 
 
 // heatmap
