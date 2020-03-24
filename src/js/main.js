bdba63cf784fd989231691ab29abd62cb199899b
celyes <celyes02@gmail.com>
2020-03-24 17:00:49 +0100

    update js files structure

--- src/js/main.js
+++ src/js/main.js
@@ -4,34 +4,13 @@ import { cases, addressPoints } from './addresses';
 jQuery(document).ready( function () { 
   $("#cases-by-wilaya").DataTable({
     data: cases,
+    language: {
+      url: "https://cdn.datatables.net/plug-ins/1.10.20/i18n/Arabic.json"
+    },
     columns: [
       { title: "الوﻻية" },
       { title: "الحالات" },
     ],
-    language: {
-        
-      sEmptyTable:     "ليست هناك بيانات متاحة في الجدول",
-      sLoadingRecords: "جارٍ التحميل...",
-      sProcessing:   "جارٍ التحميل...",
-      sLengthMenu:   "أظهر _MENU_ مدخلات",
-      sZeroRecords:  "لم يعثر على أية سجلات",
-      sInfo:         "إظهار _START_ إلى _END_ من أصل _TOTAL_ مدخل",
-      sInfoEmpty:    "يعرض 0 إلى 0 من أصل 0 سجل",
-      sInfoFiltered: "(منتقاة من مجموع _MAX_ مُدخل)",
-      sInfoPostFix:  "",
-      sSearch:       "ابحث:",
-      sUrl:          "",
-      oPaginate: {
-          sFirst:    "الأول",
-          sPrevious: "السابق",
-          sNext:     "التالي",
-          sLast:     "الأخير"
-      },
-      oAria: {
-          sSortAscending:  ": تفعيل لترتيب العمود تصاعدياً",
-          sSortDescending: ": تفعيل لترتيب العمود تنازلياً"   
-      }
-    },
     pageLength: 25,
     order: [[ 1, "desc" ]]    
   }); 
