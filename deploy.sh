f26416ad0ecd890953bb3faaf88cbd024ad23ca3
celyes <celyes02@gmail.com>
2020-03-31 14:07:25 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-
 if test -z "$1"
 then
 	echo "\e[31;1m Bundling and deployment stopped. Please provide a commit message!\e[0m"
@@ -10,3 +9,5 @@ npm run webpack
 git add .
 git commit -m "$1"
 git push origin master
+	
+echo "\e[32;1m Deployed successfully! See: https://corona-dz.now.sh\e[0m"
