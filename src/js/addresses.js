e5677ab7a3e5beca1d4b95da8dd49e95b5c9115d
celyes <celyes02@gmail.com>
2020-04-01 12:29:38 +0100

    update cases by wilaya

--- src/js/addresses.js
+++ src/js/addresses.js
@@ -3,7 +3,7 @@ let cases = require('./wilayas');
 let addressPoints = [];
 cases.forEach(x => {
     for(let i = 0; i < (x[1] * 15); i++){
-      addressPoints.push([x[2], x[3], 30]);
+      addressPoints.push([x[2], x[3], 25]);
     }
 });
 
