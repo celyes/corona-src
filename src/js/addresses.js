61d210fca85d8e110903fb9df4d6cdaa111c52b1
celyes <celyes02@gmail.com>
2020-04-12 12:42:19 +0100

    update heatmap

--- src/js/addresses.js
+++ src/js/addresses.js
@@ -2,7 +2,7 @@ let cases = require('./wilayas');
 
 let addressPoints = [];
 cases.forEach(x => {
-    for(let i = 0; i < (x[1] * 25); i++){
+    for(let i = 0; i < (x[1] * 20); i++){
       addressPoints.push([x[2], x[3], 25]);
     }
 });
